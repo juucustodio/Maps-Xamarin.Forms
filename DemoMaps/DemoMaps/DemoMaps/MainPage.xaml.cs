bcd71cf0e3fa315905bf7d9a3f21460be5855ca2
Juliano Custódio <juliano.custodio@hotmail.com.br>
2017-09-22 17:53:36 -0300

    Implements  Maps with location pin

--- DemoMaps/DemoMaps/DemoMaps/MainPage.xaml.cs
+++ DemoMaps/DemoMaps/DemoMaps/MainPage.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 
 namespace DemoMaps
 {
@@ -12,6 +8,21 @@ namespace DemoMaps
         public MainPage()
         {
             InitializeComponent();
+
+            Mapa.MoveToRegion(MapSpan.FromCenterAndRadius(
+                new Position(-23.4859591, -47.4420192),
+                Distance.FromMiles(0.5)));
+
+
+            var pin = new Pin
+            {
+                Type = PinType.Place,
+                Position = new Position(-23.4859591, -47.4420192),
+                Label = "Demo Maps",
+                Address = "www.julianocustodio.com",
+            };
+
+            Mapa.Pins.Add(pin);
         }
     }
 }
